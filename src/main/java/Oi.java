035a338b6bb5abcf253b0e9dc685258beb726dee
ayla <ayladebora@gmail.com>
2026-09-01 11:31:19 -0300

    Adicionando variáveis e método

--- src/main/java/Oi.java
+++ src/main/java/Oi.java
@@ -1,7 +1,33 @@
+import javax.swing.JOptionPane;
+
 public class Oi {
 
+    public static void mostreMensagemBoasVindas(String nome){
+        System.out.println("Oi "+ nome);
+        System.out.println("Como vai você?");
+    }
+
+
     public static void main(String [] args){
-        System.out.print("Oi");
+        String nomeLido = JOptionPane.showInputDialog("Diga seu nome");
+        mostreMensagemBoasVindas(nomeLido);
+        int x = 10;
+        String nome = "Ayla";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.charAt(3));
+
+
+        char letra = 'c';
+        double altura = 1.75;
+        float numeroReal = 8.87f;
+        boolean toComFome = true;
+        System.out.println(3+4);
+        System.out.println(3*4);
+        System.out.println(30.0/4);
+        System.out.println(15%2);
+        System.out.println(15-2);
+
+
     }
 
 
